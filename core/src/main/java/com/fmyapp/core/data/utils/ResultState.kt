01840929098eeedc281d2d15f7c113008109a1a2
fredykamaulana <fredyka.maulana@gmail.com>
2022-06-11 22:43:44 +0700

    delete unused object

--- core/src/main/java/com/fmyapp/core/data/utils/ResultState.kt
+++
@@ -1,7 +0,0 @@
-package com.fmyapp.core.data.utils
-
-sealed class ResultState<T>(val data: T? = null, val message: String? = null) {
-    class Loading<T>(data: T? = null) : ResultState<T>(data)
-    class Success<T>(data: T) : ResultState<T>(data)
-    class Error<T>(message: String? = null, data: T? = null) : ResultState<T>(data, message)
-}
